157266237ef8cda170929a1adcbcbdb1db9b1c59
mpmnath <mpm.nath007@gmail.com>
2024-02-07 16:59:39 +0100

    Bloc logic refactor and adapt UI

--- lib/view/widgets/timer/timer_widget.dart
+++ lib/view/widgets/timer/timer_widget.dart
@@ -1,26 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:odoo_apexive/bloc/new_timer/new_timer_bloc.dart';
-import 'package:odoo_apexive/models/timer_model.dart';
+import 'package:odoo_apexive/bloc/timer/timer_bloc.dart';
 import 'package:odoo_apexive/utils/utils.dart';
-import 'package:odoo_apexive/view/screens/task_screen.dart';
+import 'package:odoo_apexive/view/screens/task/task_screen.dart';
 import 'package:odoo_apexive/view/widgets/timer/timer_button.dart';
 import 'package:odoo_apexive/view/widgets/timer/timer_card.dart';
 
 class TimerWidget extends StatelessWidget {
   final int index;
-  final TimerModel timer;
   const TimerWidget({
     super.key,
     required this.index,
-    required this.timer,
   });
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return BlocBuilder<NewTimerBloc, NewTimerState>(
+    return BlocBuilder<TimerBloc, TimerState>(
       builder: (context, state) {
+        final timer = state.timers[index];
         return GestureDetector(
           onTap: () {
             Navigator.push(
@@ -36,7 +34,6 @@ class TimerWidget extends StatelessWidget {
           },
           child: Container(
             width: double.infinity,
-            //height: 100,
             margin: const EdgeInsets.symmetric(
               horizontal: 16.0,
               vertical: 8.0,
@@ -46,124 +43,51 @@ class TimerWidget extends StatelessWidget {
               borderRadius: BorderRadius.circular(16.0),
             ),
             padding: const EdgeInsets.all(16.0),
-            child: Row(
-              children: [
-                Container(
-                  width: 2,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8.0),
-                    color: const Color(0xffffc629),
+            child: IntrinsicHeight(
+              child: Row(
+                children: [
+                  Container(
+                    width: 2,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      color: const Color(0xffffc629),
+                    ),
+                    margin: const EdgeInsets.only(right: 12.0),
                   ),
-                  margin: const EdgeInsets.only(right: 12.0),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TimerCard(
-                        title: timer.task.name,
-                        icon: Icons.star_outline,
-                        textStyle: theme.textTheme.titleMedium,
-                      ),
-                      const SizedBox(height: 2.0),
-                      TimerCard(
-                        title: timer.project.name,
-                        icon: Icons.work_outline_outlined,
-                        textStyle: theme.textTheme.bodyMedium,
-                      ),
-                      const SizedBox(height: 2.0),
-                      TimerCard(
-                        title: 'Deadline ${timer.task.deadline.formattedDate}',
-                        icon: Icons.access_time_outlined,
-                        textStyle: theme.textTheme.bodyMedium,
-                      ),
-                    ],
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TimerCard(
+                          title: timer.task.name,
+                          icon: timer.isFavorite
+                              ? Icons.star
+                              : Icons.star_outline,
+                          textStyle: theme.textTheme.titleMedium,
+                        ),
+                        const SizedBox(height: 2.0),
+                        TimerCard(
+                          title: timer.project.name,
+                          icon: Icons.work_outline_outlined,
+                          textStyle: theme.textTheme.bodyMedium,
+                        ),
+                        const SizedBox(height: 2.0),
+                        TimerCard(
+                          title:
+                              'Deadline ${timer.task.deadline.formattedDateSlash}',
+                          icon: Icons.access_time_outlined,
+                          textStyle: theme.textTheme.bodyMedium,
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                const SizedBox(width: 8.0),
-                ...switch (state) {
-                  NewTimerInitial() => {
-                      TimerButton(
-                        duration: state.timers[index].elapsedTime,
-                        icon: Icons.play_arrow,
-                        onPressed: () {
-                          context.read<NewTimerBloc>().add(
-                                NewTimerStarted(index: index),
-                              );
-                        },
-                      ),
-                    },
-                  NewTimerRunInProgress() => [
-                      TimerButton(
-                        duration: state.timer.elapsedTime,
-                        icon: Icons.pause,
-                        onPressed: () {
-                          context.read<NewTimerBloc>().add(
-                                NewTimerPaused(index: index),
-                              );
-                        },
-                      ),
-                    ],
-                  NewTimerPause() => [
-                      TimerButton(
-                        duration: state.timers[index].elapsedTime,
-                        icon: Icons.play_arrow,
-                        onPressed: () {
-                          context.read<NewTimerBloc>().add(
-                                NewTimerResumed(index: index),
-                              );
-                        },
-                      ),
-                    ],
-                  NewTimerStop() => [
-                      TimerButton(
-                        duration: state.timers[index].elapsedTime,
-                        icon: Icons.play_arrow,
-                        onPressed: () {
-                          context.read<NewTimerBloc>().add(
-                                NewTimerStarted(index: index),
-                              );
-                        },
-                      ),
-                    ],
-                  // TODO: Handle this case.
-                  NewTimerStart() => [
-                      TimerButton(
-                        duration: state.timers[index].elapsedTime,
-                        icon: Icons.play_arrow,
-                        onPressed: () {
-                          context.read<NewTimerBloc>().add(
-                                NewTimerStarted(index: index),
-                              );
-                        },
-                      ),
-                    ],
-                  // TODO: Handle this case.
-                  NewTimerResume() => [
-                      TimerButton(
-                        duration: state.timers[index].elapsedTime,
-                        icon: Icons.pause,
-                        onPressed: () {
-                          context.read<NewTimerBloc>().add(
-                                NewTimerPaused(index: index),
-                              );
-                        },
-                      ),
-                    ],
-                  // TODO: Handle this case.
-                  NewTimerLoaded() => [
-                      TimerButton(
-                        duration: state.timers[index].elapsedTime,
-                        icon: Icons.play_arrow,
-                        onPressed: () {
-                          context.read<NewTimerBloc>().add(
-                                NewTimerStarted(index: index),
-                              );
-                        },
-                      ),
-                    ],
-                },
-              ],
+                  const SizedBox(width: 8.0),
+                  TimerButton(
+                    index: index,
+                    timer: timer,
+                  ),
+                ],
+              ),
             ),
           ),
         );
@@ -171,68 +95,3 @@ class TimerWidget extends StatelessWidget {
     );
   }
 }
-
-
-
-/*
-...[
-                  if (state is TimerInitial) 
-                  context.read<TimerBloc>().add(
-                            TimerStarted(timerId: timerId, timer: timer),
-                          ),
-                    TimerButton(
-                      timer: timer,
-                      duration: timer.elapsedTime,
-                      icon: Icons.play_arrow,
-                      onPressed: () {
-                        context.read<TimerBloc>().add(
-                              TimerStarted(
-                                timerId: timerId,
-                                timer: timer,
-                              ),
-                            );
-                      },
-                    ),
-                  if (state is TimerRunInProgress)
-                    TimerButton(
-                      timer: state.timer,
-                      duration: state.timer.elapsedTime,
-                      icon: Icons.pause,
-                      onPressed: () {
-                        context.read<TimerBloc>().add(
-                              TimerPaused(
-                                timerId: timerId,
-                                timer: state.timer,
-                              ),
-                            );
-                      },
-                    ),
-                  if (state is TimerPause)
-                    TimerButton(
-                      timer: state.timer,
-                      duration: state.timer.elapsedTime,
-                      icon: Icons.play_arrow,
-                      onPressed: () {
-                        context.read<TimerBloc>().add(
-                              TimerResumed(
-                                timerId: timerId,
-                                timer: state.timer,
-                              ),
-                            );
-                      },
-                    ),
-                  TimerButton(
-                    timer: timer,
-                    duration: timer.elapsedTime,
-                    icon: Icons.play_arrow,
-                    onPressed: () {
-                      context.read<TimerBloc>().add(
-                            TimerStarted(
-                              timerId: timerId,
-                              timer: timer,
-                            ),
-                          );
-                    },
-                  ),
-                ],
- */
