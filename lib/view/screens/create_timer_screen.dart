e4f505ecac71046d54a2e658d603abef32d49c2c
mpmnath <mpm.nath007@gmail.com>
2024-02-07 16:56:40 +0100

    Refactor and adapt new bloc

--- lib/view/screens/create_timer_screen.dart
+++ lib/view/screens/create_timer_screen.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:odoo_apexive/bloc/new_timer/new_timer_bloc.dart';
+import 'package:odoo_apexive/bloc/timer/timer_bloc.dart';
 import 'package:odoo_apexive/models/project_model.dart';
 import 'package:odoo_apexive/models/task_model.dart';
 import 'package:odoo_apexive/models/timer_model.dart';
 import 'package:odoo_apexive/theme/palette.dart';
 import 'package:odoo_apexive/utils/data.dart';
-import 'package:uuid/uuid.dart';
 
 class CreateTimerScreen extends StatefulWidget {
   const CreateTimerScreen({super.key});
@@ -25,9 +24,11 @@ class _CreateTimerScreenState extends State<CreateTimerScreen> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: theme.colorScheme.background,
       extendBodyBehindAppBar: true,
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text(
           'Create Timer',
@@ -38,6 +39,7 @@ class _CreateTimerScreenState extends State<CreateTimerScreen> {
       ),
       body: Container(
         width: double.infinity,
+        height: size.height,
         decoration: const BoxDecoration(
           gradient: ColorPalette.lightSurface,
         ),
@@ -45,94 +47,87 @@ class _CreateTimerScreenState extends State<CreateTimerScreen> {
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
+              mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Column(
-                  children: [
-                    DropdownButtonFormField(
-                      icon: const Icon(
-                        Icons.keyboard_arrow_down_outlined,
-                        size: 32,
-                      ),
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                      ),
-                      value: _selectedProject,
-                      items: projectList.map((project) {
+                DropdownButtonFormField(
+                  value: _selectedProject,
+                  icon: const Icon(
+                    Icons.keyboard_arrow_down_outlined,
+                    size: 32,
+                  ),
+                  style: theme.textTheme.bodyLarge!,
+                  dropdownColor: theme.colorScheme.surface,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                  items: projectList.map((project) {
+                    return DropdownMenuItem(
+                      value: project,
+                      child: Text(project.name),
+                    );
+                  }).toList(),
+                  onChanged: (ProjectModel? project) {
+                    setState(() {
+                      _selectedProject = project!;
+                      _selectedTask =
+                          project.tasks.isNotEmpty ? project.tasks[0] : null;
+                    });
+                  },
+                  hint: Text(
+                    'Project',
+                    style: theme.textTheme.bodyLarge,
+                  ),
+                ),
+                //Supporting text is disabled
+                const Text(''),
+                const SizedBox(height: 4.0),
+                DropdownButtonFormField(
+                  value: _selectedTask,
+                  style: theme.textTheme.bodyLarge!,
+                  dropdownColor: theme.colorScheme.surface,
+                  icon: const Icon(
+                    Icons.keyboard_arrow_down_outlined,
+                    size: 32,
+                  ),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                  items: _selectedProject?.tasks.map((task) {
                         return DropdownMenuItem(
-                          value: project,
-                          child: Text(project.name),
+                          value: task,
+                          child: Text(task.name),
                         );
-                      }).toList(),
-                      onChanged: (ProjectModel? project) {
-                        setState(() {
-                          _selectedProject = project!;
-                          _selectedTask = project.tasks.isNotEmpty
-                              ? project.tasks[0]
-                              : null;
-                        });
-                      },
-                      hint: Text(
-                        'Project',
-                        style: theme.textTheme.bodyLarge,
-                      ),
-                    ),
-                    //Supporting text is disabled
-                    const Text(''),
-                  ],
+                      }).toList() ??
+                      [],
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedTask = value! as TaskModel?;
+                    });
+                  },
+                  hint: Text(
+                    'Task',
+                    style: theme.textTheme.bodyLarge,
+                  ),
                 ),
+                const Text(''),
                 const SizedBox(height: 4.0),
-                Column(
-                  children: [
-                    DropdownButtonFormField(
-                      value: _selectedTask,
-                      icon: const Icon(
-                        Icons.keyboard_arrow_down_outlined,
-                        size: 32,
-                      ),
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                      ),
-                      items: _selectedProject?.tasks.map((task) {
-                            return DropdownMenuItem(
-                              value: task,
-                              child: Text(task.name),
-                            );
-                          }).toList() ??
-                          [],
-                      onChanged: (value) {
-                        setState(() {
-                          _selectedTask = value! as TaskModel?;
-                        });
-                      },
-                      hint: Text(
-                        'Task',
-                        style: theme.textTheme.bodyLarge,
-                      ),
+                TextField(
+                  controller: _descriptionTextController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
                     ),
-                    const Text(''),
-                  ],
+                    hintText: 'Description',
+                    hintStyle: theme.textTheme.bodyLarge,
+                  ),
                 ),
-                const SizedBox(height: 4.0),
-                Column(
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                        hintText: 'Description',
-                        hintStyle: theme.textTheme.bodyLarge,
-                      ),
-                    ),
-                    //Supporting text is disabled
-                    const Text(''),
-                  ],
-                ),
+                //Supporting text is disabled
+                const Text(''),
                 const SizedBox(height: 4.0),
                 CheckboxListTile(
                   contentPadding: EdgeInsets.zero,
@@ -156,34 +151,35 @@ class _CreateTimerScreenState extends State<CreateTimerScreen> {
                   ),
                 ),
                 const Spacer(),
-                BlocListener<NewTimerBloc, NewTimerState>(
+                BlocListener<TimerBloc, TimerState>(
                   listener: (context, state) {},
                   child: FilledButton(
                     onPressed: () {
-                      print('Create Timer');
+                      if (_selectedProject == null || _selectedTask == null) {
+                        return;
+                      }
 
-                      final timerId = const Uuid().v4();
-                      print("Creating new Timer Id: $timerId");
-                      final newTimer = TimerModel(
-                        id: context.read<NewTimerBloc>().state.timers.length,
+                      final timer = TimerModel(
+                        id: context.read<TimerBloc>().state.timers.length,
                         description: _descriptionTextController.text,
                         isFavorite: _isFavorite,
                         isRunning: false,
+                        isStopped: false,
                         startTime: DateTime.now(),
                         project: _selectedProject!,
                         task: _selectedTask!,
                         elapsedTime: _selectedTask!.duration,
                       );
 
-                      context.read<NewTimerBloc>().add(
-                            NewTimerCreated(
-                              newTimer: newTimer,
+                      context.read<TimerBloc>().add(
+                            TimerCreated(
+                              timer: timer,
                             ),
                           );
                       Navigator.pop(context);
                     },
                     style: FilledButton.styleFrom(
-                      backgroundColor: theme.colorScheme.secondary,
+                      backgroundColor: theme.colorScheme.secondaryContainer,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(12.0),
                       ),
@@ -192,7 +188,12 @@ class _CreateTimerScreenState extends State<CreateTimerScreen> {
                         horizontal: 20.0,
                       ),
                     ),
-                    child: const Text('Create Timer'),
+                    child: Text(
+                      'Create Timer',
+                      style: theme.textTheme.labelLarge!.copyWith(
+                        color: theme.colorScheme.onSecondaryContainer,
+                      ),
+                    ),
                   ),
                 ),
               ],
