e781338475c70b98cc8b9b972fab07626ecc3882
mpmnath <mpm.nath007@gmail.com>
2024-02-07 16:58:59 +0100

    Logic refactored for optimization

--- lib/view/widgets/timer/timer_button.dart
+++ lib/view/widgets/timer/timer_button.dart
@@ -1,53 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:odoo_apexive/bloc/timer/timer_bloc.dart';
+import 'package:odoo_apexive/models/timer_model.dart';
 import 'package:odoo_apexive/utils/utils.dart';
 
 class TimerButton extends StatelessWidget {
   const TimerButton({
     super.key,
-    required this.duration,
-    required this.icon,
-    required this.onPressed,
+    required this.index,
+    required this.timer,
   });
 
-  final Duration duration;
-  final IconData icon;
-  final void Function() onPressed;
+  final int index;
+  final TimerModel timer;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return IconButton(
-      onPressed: onPressed,
-      padding: const EdgeInsets.symmetric(
-        vertical: 8.0,
-        horizontal: 20.0,
-      ),
-      style: IconButton.styleFrom(
-        backgroundColor: theme.colorScheme.secondary,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(64.0),
-        ),
-      ),
-      icon: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          SizedBox(
-            width: 44,
-            child: Text(
-              formatDuration(duration),
-              style: theme.textTheme.labelLarge,
+    return timer.elapsedTime.inSeconds == 0
+        ? const SizedBox()
+        : IconButton(
+            onPressed: () {
+              if (timer.isRunning) {
+                context.read<TimerBloc>().add(
+                      TimerPaused(index: index),
+                    );
+              } else {
+                context.read<TimerBloc>().add(
+                      TimerStarted(index: index),
+                    );
+              }
+            },
+            padding: const EdgeInsets.symmetric(
+              vertical: 8.0,
+              horizontal: 20.0,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(4.0),
-            child: Icon(
-              icon,
-              size: 32,
-              color: theme.colorScheme.onSecondaryContainer,
+            style: IconButton.styleFrom(
+              backgroundColor: timer.isFavorite
+                  ? theme.colorScheme.primary
+                  : theme.colorScheme.secondary,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(64.0),
+              ),
             ),
-          ),
-        ],
-      ),
-    );
+            icon: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: 44,
+                  child: Text(
+                    formatDurationToMS(timer.elapsedTime),
+                    style: theme.textTheme.labelLarge!.copyWith(
+                      color: timer.isFavorite
+                          ? theme.colorScheme.onPrimary
+                          : theme.colorScheme.onSecondary,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Icon(
+                    timer.isRunning ? Icons.pause : Icons.play_arrow,
+                    size: 32,
+                    color: timer.isFavorite
+                        ? theme.colorScheme.onPrimary
+                        : theme.colorScheme.onSecondary,
+                  ),
+                ),
+              ],
+            ),
+          );
   }
 }
