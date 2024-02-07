6481aae554ee012006dce5e3cdc46dcb6572d6cc
mpmnath <mpm.nath007@gmail.com>
2024-02-07 16:55:38 +0100

    Updated util functions - HHMMSS added

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,10 +1,29 @@
+//Extension on DateTime to format date to MM/DD/YYYY and MM.DD.YYYY
 extension DateTimeExtension on DateTime {
-  String get formattedDate {
+  String get formattedDateSlash {
     return '${month.toString().padLeft(2, "0")}/${day.toString().padLeft(2, "0")}/$year';
+  }
+
+  String get formattedDateDot {
+    return '${month.toString().padLeft(2, "0")}.${day.toString().padLeft(2, "0")}.$year';
   }
 }
 
-String formatDuration(Duration duration) {
+//Formats duration to HH:MM:SS
+String formatDurationToHMS(Duration duration) {
+  String twoDigits(int n) {
+    if (n >= 10) return "$n";
+    return "0$n";
+  }
+
+  final hours = twoDigits(duration.inHours.remainder(60));
+  final minutes = twoDigits(duration.inMinutes.remainder(60));
+  final seconds = twoDigits(duration.inSeconds.remainder(60));
+  return "$hours:$minutes:$seconds";
+}
+
+//Format duration to MM:SS
+String formatDurationToMS(Duration duration) {
   String twoDigits(int n) {
     if (n >= 10) return "$n";
     return "0$n";
